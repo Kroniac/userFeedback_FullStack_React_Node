0edd19c91ef564c03a33289ee1d40ff5adbbb6ea
Farid Ansari <farid12ansari7@gmail.com>
2017-11-26 23:38:40 +0530

    mongoDatabase Uri setup via mlab

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 require('./services/passport'); //as nothing is returned but only want to execute this file.So nothing to store in a variable
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
+
+mongoose.connect(keys.mongoUri);
 
 const app = express();
 authRoutes(app);
