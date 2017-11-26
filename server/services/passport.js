952775429edfc1bcc87261b0b046047b0eccf56c
Farid Ansari <farid12ansari7@gmail.com>
2017-11-27 01:57:17 +0530

    to save only one user of a id

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,7 @@ const keys = require('../config/keys');
 
 const users = mongoose.model('user');
 
+//for getting google user info like its id and store in it mongodb
 passport.use(
   new googleStrategy(
     {
@@ -13,7 +14,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new users({ googleId: profile.id }).save();
+      users.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //already have a record with this userID, no need to create a new one
+          done(null, existingUser); // to tell the passportjs that the user is there and it should resume the auth process
+        } else {
+          //no record with this userID, need to make a new one
+          new users({ googleId: profile.id })
+            .save() //to make and save recored with userID: existingUser
+            .then(user => done(null, user)); //to tell the passportjs that the user is created and it should resume the auth process
+        }
+      });
     }
   )
 );
