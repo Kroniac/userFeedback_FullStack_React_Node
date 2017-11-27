b289b3cc9d0e926f5db0f895657fdc390fa26a33
Farid Ansari <farid12ansari7@gmail.com>
2017-11-27 09:08:25 +0530

    logging users out

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,6 +9,12 @@ module.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  /* req.logout() takes the cookie and kills the id in there, thus making the user no longer authenticated*/
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
