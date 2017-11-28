4a773184a31490b2a285d21780f1e32f77c58c74
Farid Ansari <farid12ansari7@gmail.com>
2017-11-28 11:51:22 +0530

    set different keys for development and production

---
+++ server/config/keys.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV === 'production') {
+  //we are in the production mode
+  module.exports = require('./prod');
+} else {
+  // we are in  the development mode- return the dev keys
+  module.exports = require('./dev');
+}
