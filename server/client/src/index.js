2018b24faf6b05635ef42a0dbd3569ee72f8dd38
Farid Ansari <farid12ansari7@gmail.com>
2017-11-29 10:10:51 +0530

    actionCreator for fetchUser and header added

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'materialize-css/dist/css/materialize.min.css';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './store/reducers/index';
 import registerServiceWorker from './registerServiceWorker';
+import reduxThunk from 'redux-thunk';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(rootReducer, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
