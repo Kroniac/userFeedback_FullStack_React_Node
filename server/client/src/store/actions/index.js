ea0b43cced471163d00ac38df96629731be268e3
Farid Ansari <farid12ansari7@gmail.com>
2017-11-29 11:07:13 +0530

    used async/await insted of promise

--- server/client/src/store/actions/index.js
+++ server/client/src/store/actions/index.js
@@ -1,13 +1,10 @@
 import * as actionTypes from './actionTypes';
 import axios from 'axios';
 
-export const fetchUsers = () => {
-  return dispatch => {
-    axios.get('/api/current_user').then(res =>
-      dispatch({
-        type: actionTypes.FETCH_USERS,
-        payload: res
-      })
-    );
-  };
+export const fetchUsers = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+  dispatch({
+    type: actionTypes.FETCH_USERS,
+    payload: res
+  });
 };
