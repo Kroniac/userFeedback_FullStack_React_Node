7dda2696894f49c138b9fcba6b9c76766575dac8
Farid Ansari <farid12ansari7@gmail.com>
2017-12-01 01:45:52 +0530

    stripe token handled by reducer

--- server/client/src/store/actions/index.js
+++ server/client/src/store/actions/index.js
@@ -1,7 +1,13 @@
 import * as actionTypes from './actionTypes';
 import axios from 'axios';
 
+//to fetch user's login data
 export const fetchUsers = () => async dispatch => {
   const res = await axios.get('/api/current_user');
+  dispatch({ type: actionTypes.FETCH_USERS, payload: res.data });
+};
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
   dispatch({ type: actionTypes.FETCH_USERS, payload: res.data });
 };
