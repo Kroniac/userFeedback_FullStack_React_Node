5adfc2e07fffa2996b6f6652bf3161871201a453
Farid Ansari <farid12ansari7@gmail.com>
2017-12-02 04:03:37 +0530

    debugging for heroku

---
+++ server/config/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientId:
+      '551191656761-t00anvqtcovgj98a4tdut46661nm863j.apps.googleusercontent.com',
+    googleClientSecret: 'zeowJ-JjkAiw2K1UR37OkIeW',
+    mongoUri:
+      'mongodb://farid:amdradeon@ds121716.mlab.com:21716/emaily-farid-dev',
+    cookieKey: 'fghjouyghbjiohjbnjihjbnjhbjnjknjnm,kljnkjn',
+    stripePublishableKey: 'pk_test_OAhJriGn8Ul2tBs05I047BEY',
+    stripeSecretey: 'sk_test_CbsmqWwOWJ3gk3gsYsCbL3lA'
+  };
+  
