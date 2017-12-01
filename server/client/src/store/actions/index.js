c7f14af082e9fb4c726e82d12beaa6f118bbc62a
Farid Ansari <farid12ansari7@gmail.com>
2017-12-02 02:57:30 +0530

    for debug on heroku

--- server/client/src/store/actions/index.js
+++ server/client/src/store/actions/index.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 //to fetch user's login data
 export const fetchUsers = () => async dispatch => {
   const res = await axios.get('/api/current_user');
+  console.log(res.data);
   dispatch({ type: actionTypes.FETCH_USERS, payload: res.data });
 };
 
