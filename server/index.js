2d2782167785790d86d1c88f979afc97556b6282
Farid Ansari <farid12ansari7@gmail.com>
2017-12-01 17:14:42 +0530

    set production requirement for client side for heroku

--- server/index.js
+++ server/index.js
@@ -32,6 +32,19 @@ app.use(passport.session());
 authRoutes(app); // for handling different routes
 billingRoutes(app); //for handling routes related to billing
 
+if (process.env.NODE_ENV === 'production') {
+  //express will serve up production assets
+  // like main.js file, main.css file
+  app.use(express.static('client/build'));
+
+  //express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
