1ff9ce08de0e8ab265685f1421d68cc45fd9c7ed
Farid Ansari <farid12ansari7@gmail.com>
2017-12-11 01:53:50 +0530

    set the mail details(from,subject,body,recipients)

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -2,6 +2,26 @@ const sendGrid = require('sendgrid');
 const helper = sendGrid.helper;
 const keys = require('../config/keys');
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+    //to set the email
+    //from_email : who send the email
+    //subject : subject of the email
+    //body: body of the email
+    //recipients: who to send the email
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+
+  //to return email of the recipients from the recipients sub-document
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+}
 
 module.exports = Mailer;
