262d2185a3adaeab69defb735cd773cb1ac03278
Farid Ansari <farid12ansari7@gmail.com>
2017-12-11 04:09:16 +0530

    set send function to send all properties of mail in JSON to sendGrid

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -6,6 +6,8 @@ const keys = require('../config/keys');
 class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
+
+    this.sgApi = sendgrid(keys.sendGridKey);
     //to set the email
     //from_email : who send the email
     //subject : subject of the email
@@ -47,6 +49,17 @@ class Mailer extends helper.Mail {
       personalize.addTo(recipient);
     });
     this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = this.sgApi.API(request);
+    return response;
   }
 }
 
